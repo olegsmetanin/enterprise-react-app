a42a232e3bec9cc44c84bc2b8c9c2d99589d1240
Oleg Smetanin <oleg@smetan.in>
2017-01-10 00:06:44 +0300

    fix storybook

--- packages/components/.storybook/config.js
+++ packages/components/.storybook/config.js
@@ -2,7 +2,7 @@ import { configure } from '@kadira/storybook'
 import '../src/styles/styles.less'
 
 function loadStories() {
-    require('../src/__stories__/index.story.tsx')
+    require('../src/components/__stories__/index.story.tsx')
 }
 
 configure(loadStories, module);
