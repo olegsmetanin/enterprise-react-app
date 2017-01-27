11a64c4ecaf5217a77936308b8005b818f39e141
Oleg Smetanin <oleg@smetan.in>
2017-01-27 23:18:31 +0300

    module refactoring

---
+++ packages/webclient/src/index.tsx
@@ -0,0 +1,19 @@
+/* tslint:disable */
+import * as React from "react";
+/* tslint:enable */
+import * as ReactDOM from "react-dom";
+// import * as components from 'components'
+// import {Button} from 'components'
+import {Button} from 'enterprise-react-app-webcomponents'
+
+//import from './styles/default.less'
+require('./styles/default.less')
+
+// let Button = components.Button
+ReactDOM.render(<div>QWE
+    <Button
+      onClick={() => console.log('qwe')}
+    >
+      Button
+    </Button>
+</div>, document.getElementById('app'))
